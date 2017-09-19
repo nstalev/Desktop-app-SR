34e9b4e9b780c606f6aaf0862787b945269904a6
nstalev <stalev_a@abv.bg>
2017-09-19 08:16:53 +0300

    add new projects

---
+++ SR/SR.Service/Service.cs
@@ -0,0 +1,19 @@
+﻿using SR.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR.Service
+{
+    public abstract class Service
+    {
+        public Service()
+        {
+            this.Context = new SRContext();
+        }
+        protected SRContext Context { get; }
+
+    }
+}
