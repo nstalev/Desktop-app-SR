6dd751b279367b87ee5770a3d478a816dee393ce
nstalev <stalev_a@abv.bg>
2017-09-24 08:44:46 +0300

    with new table - manipulations

--- SR/SR/Program.cs
+++ SR/SR/Program.cs
@@ -11,6 +11,7 @@ namespace SR
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
         [STAThread]
         static void Main()
         {
